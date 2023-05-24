277fcfbe18a800d29f4f827d4c4326b6d67ddee5
brettmadrid <brettmadrid9@gmail.com>
2023-05-24 16:59:41 -0700

    created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Hello from footer</footer>;
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>Amy's Recipes</span> Built by{" "}
+        <span>Brett Madrid </span>with <a href="https://gatsbyjs.com">Gatsby</a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,11 @@ import React from "react";
 import Layout from "../components/Layout";
 
 const Error = () => {
-  return <Layout>Error page</Layout>;
+  return (
+    <Layout>
+      <h1>Error page</h1>
+    </Layout>
+  );
 };
 
 export default Error;
