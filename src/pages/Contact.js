2145e0eca4b1712eed7cae69824b1777b3203ff0
brettmadrid <brettmadrid9@gmail.com>
2023-05-24 21:34:08 -0700

    Contact Form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,41 @@ import Layout from "../components/Layout";
 const Contact = () => {
   return (
     <Layout>
-      <h2>Contact Page</h2>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to get in touch?</h3>
+            <p>
+              This is the collection of recipes that I use to feed my family. I
+              did not create the recipes. However, I do substitute ingredients
+              due to food allergies and taste. If you have any questions or
+              ideas feel free to reach out. Most of these recipes are from{" "}
+              <span>
+                <a href="https://allrecipes.com">AllRecipes.com</a>
+              </span>
+            </p>
+          </article>
+          <article>
+            <form action="" className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   );
 };
