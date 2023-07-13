d69af449daf76392dd896b30c88206fcd037116e
brettmadrid <brettmadrid9@gmail.com>
2023-07-13 11:08:05 -0700

    display recipes on tags page

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { graphql } from "gatsby";
+import RecipesList from "../components/RecipesList";
+import Layout from "../components/Layout";
 
-const TagTemplate = () => {
-  return <div>tag template page</div>;
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes;
+  const tagName = pageContext.tag;
+
+  return (
+    <Layout>
+      <main className="page">
+        <h2>{tagName}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes} />
+        </div>
+      </main>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      sort: { title: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        id
+        title
+        prepTime
+        cookTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`;
 
 export default TagTemplate;
