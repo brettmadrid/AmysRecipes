9c5a7c6ee36f085af82c206edbcb9d60d0707938
brettmadrid <brettmadrid9@gmail.com>
2023-07-13 19:32:01 -0700

    submit form with formspree

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -23,7 +23,11 @@ const Contact = ({ data }) => {
             </p>
           </article>
           <article>
-            <form action="" className="form contact-form">
+            <form
+              className="form contact-form"
+              action="https://formspree.io/f/xqkvkjjj"
+              method="POST"
+            >
               <div className="form-row">
                 <label htmlFor="name">your name</label>
                 <input type="text" name="name" id="name" />
